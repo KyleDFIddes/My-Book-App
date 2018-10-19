8b9eb46cbc1eb52c072794b383c99c18b0e66a06
KyleDFIddes <43876976+KyleDFIddes@users.noreply.github.com>
2018-10-19 09:52:08 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// GRAB THE DOM ELEMENT/TAG
-var title = document.querySelector("#Title");
-// CHANGE THE HTML PF THE TAG
-title.innerHTML = "My Books App</u>";
-title.style.color = "6a6a6a";
-title.style.fontSize = "50px";
-
-var changeTitleBtn = document.querySelector("#ChangeTitleBtn");
