a17eb34822c75b18e49f8372c3b1e59ef9074937
KyleDFIddes <43876976+KyleDFIddes@users.noreply.github.com>
2018-10-19 11:01:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// GRAB THE DOM ELEMENT/TAG
+var title = document.querySelector("#Title")
+title.innerHTML = "My Books App</u>";
+title.style.color = "6a6a6a";
+title.style.fontSize = "50px";
+
+var changeTitleBtn = document.querySelector("#ChangeTitleBtn");
